335f547129fcc604ef85877ec2d0692ceda8f78d
ItsSavo <77900070+ItsSavo@users.noreply.github.com>
2022-10-17 22:59:01 +0100

    DONEZO

--- Assets/Scripts/Retry.cs
+++ Assets/Scripts/Retry.cs
@@ -8,7 +8,7 @@ public class Retry : MonoBehaviour
     public void TryAgain()
     {
         
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void Bye()
